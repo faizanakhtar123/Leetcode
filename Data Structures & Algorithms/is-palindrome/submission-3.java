fe3a8a6ccfe28e7239a7fdab6010a238cf9bb34b
Faizan Akhtar <faizanakhtar83jpr@gmail.com>
2026-08-22 22:26:08 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int i = 0 , j = s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetter(s.charAt(i))) i++;
+            while(i<j && !Character.isLetter(s.charAt(j))) j--;
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j)))
+            return false;
+             i++; j--;
+        }
+        return true;
+    }
+}
